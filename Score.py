df273034fe9d6ff478885c3babb1ee71308a7e7e
orelrahum <orel.rahum@gmail.com>
2022-02-06 22:14:35 +0200

    added Utils ,Score , MainScores

---
+++ Score.py
@@ -0,0 +1,19 @@
+from Utils import *
+
+
+def add_score(difficulty):
+    '''
+    The function add score to txt file or create new score file.
+    '''
+    points_of_winning = (int(difficulty) * 3) + 5
+    score_file = open(SCORES_FILE_NAME, 'r')
+    points_of_winning = int(score_file.read()) + (int(difficulty) * 3) + 5
+    score_file.close()
+    score_file = open(SCORES_FILE_NAME, 'w')
+    score_file.write(str(points_of_winning))
+    score_file.close()
+    return points_of_winning
+
+
+if __name__ == '__main__':
+    add_score('3')
